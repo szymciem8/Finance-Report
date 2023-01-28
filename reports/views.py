5aa699cdf623fbf45c81bb13c7dc4797e88edb66
Paweł Wojaczek <pwojx@protonmail.com>
2023-01-28 18:10:22 +0100

    Improve report page, add current price and about, improve chart

--- reports/views.py
+++ reports/views.py
@@ -11,16 +11,32 @@ from django.contrib.auth.decorators import login_required
 class FinanceReportView(View):
 
     # @login_required
-    def get(self, request, stock='AAPL'):
+    def get(self, request, stock='AAPL', period='1mo'):
+        stock = request.GET.get('stock', 'AAPL').upper()
+        period = request.GET.get('period', '1mo')
+        valid_periods = ['1d', '5d', '1mo', '3mo', '6mo', '1y', '2y', '5y', '10y', 'ytd', 'max']
+        if period not in valid_periods:
+            period = '1mo'
 
-        today = datetime.date.today().strftime('%Y-%m-%d')
-        two_weeks_ago = (datetime.date.today() - datetime.timedelta(days=14)).strftime('%Y-%m-%d')
+        ticker = yf.Ticker(stock)
 
-        df_yahoo = yf.download([stock], start=two_weeks_ago, end=today, progress=False)
+        df_yahoo = ticker.history(period=period)
+        info_yahoo = ticker.info
         
         open_price = df_yahoo['Open'].values
+        high_price = df_yahoo['High'].values
+        low_price = df_yahoo['Low'].values
         close_price = df_yahoo['Close'].values
+        volume = df_yahoo['Volume'].values
         date = df_yahoo.index.values
+
+        short_name = info_yahoo.get('shortName', 'N/A')
+        long_name = info_yahoo.get('longName', 'N/A')
+        sector = info_yahoo.get('sector', 'N/A')
+        website = info_yahoo.get('website', 'N/A')
+        long_summary = info_yahoo.get('longBusinessSummary', 'N/A')
+
+        current_price = ticker.analyst_price_target[0]['currentPrice']
 
         # Convert numpy datetime64 to datetime
         date = [pd.to_datetime(d).date().strftime('%Y-%m-%d') for d in date]
@@ -29,7 +45,16 @@ class FinanceReportView(View):
             'stock': str(stock),
             'date': list(date),
             'open_price': list(open_price),
-            'close_price': list(close_price)
+            'high_price': list(high_price),
+            'low_price': list(low_price),
+            'close_price': list(close_price),
+            'volume': list(volume),
+            'short_name': str(short_name),
+            'long_name': str(long_name),
+            'sector': str(sector),
+            'website': str(website),
+            'long_summary': str(long_summary),
+            'current_price': f'{current_price:.2f}'
         }
 
         return render(request, 'general_report_test.html', context=context)
